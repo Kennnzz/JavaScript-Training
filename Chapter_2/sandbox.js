bdf65013f290fd1890afe80348c7f1220b985112
Kenneth Agustin <kagustin2@rrc.ca>
2024-01-04 16:20:00 -0600

    added more comments to explain

--- Chapter_2/sandbox.js
+++ Chapter_2/sandbox.js
@@ -1,7 +1,18 @@
+// Let variable. 'Let' is able to be overwritten
 let age = 25;
 let year = 2019;
 
+// More than 1 variable in a log
 console.log(age, year);
 
+// age does not need let
+// as it's alread been declared
 age = 30;
 console.log(age);
+
+// Const variable is unable to be overwritten
+const points = 100;
+console.log(points);
+
+var score = 75;
+console.log(score);
