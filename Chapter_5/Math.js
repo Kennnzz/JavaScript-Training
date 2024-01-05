ca349f8801de6bc6f3a311bc9f25605cfd649b44
Kenneth Agustin <kagustin2@rrc.ca>
2024-01-05 13:26:44 -0600

    saved math training

---
+++ Chapter_5/Math.js
@@ -0,0 +1,17 @@
+// Math object
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E)
+
+const area = 7.7;
+
+console.log(Math.round(area)); // round
+console.log(Math.floor(area)); // always round down
+console.log(Math.ceil(area)); // always round up
+console.log(Math.trunc(area)); // leaves the integer
+
+// random numbers
+const random = Math.random()
+console.log(random);
+console.log(Math.round(random * 100));
