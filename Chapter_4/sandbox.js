1f4686714943bdad1366c2229947cd076ede165e
Kenneth Agustin <kagustin2@rrc.ca>
2024-01-05 11:23:11 -0600

    callback arrow functions training

--- Chapter_4/sandbox.js
+++ Chapter_4/sandbox.js
@@ -1,14 +1,40 @@
-const name = 'shaun';
+// callbacks & foreach
 
-// functions
+// const myFunc = (callbackFunc) => {
+//     // do something 
+//     let value = 50;
+//     callbackFunc(value);
+// };
 
-const greet = () => 'hello';
+// Passing in a function as an argument
+// myFunc(function(value){
+//     // do something
+//     console.log(value);
+// });
 
-let resultOne = greet();
-console.log(resultOne);
+// Arrow function version
+// myFunc(value => {
+//     // do something
+//     console.log(value);
+// });
 
-// methods
 
-let resultTwo = name.toUpperCase();
+// callbacks & foreach
+let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+const logPerson = (person, index) => {
+    console.log(`${index} - hello ${person}`);
+};
 
-console.log(resultTwo)
+// forEach function iterates over an array.
+// basically saying "for each element in that array, it will call that callback function"
+// people.forEach(function(person){ // used 'person' because it is the 'singular' word of whatever 'people' is.
+//     console.log(person);
+// });
+
+// Converting to an arrow function
+// people.forEach((person, index) => {
+//     console.log(index, person);
+// }); // instead of doing this
+
+// do it like this
+people.forEach(logPerson)
