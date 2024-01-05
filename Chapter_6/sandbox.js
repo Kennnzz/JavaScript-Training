ca0a43d6c4706305cb5ffc1cb1dfedafd5959312
Kenneth Agustin <kagustin2@rrc.ca>
2024-01-05 15:19:37 -0600

    appending and overwriting training

--- Chapter_6/sandbox.js
+++ Chapter_6/sandbox.js
@@ -1,19 +1,23 @@
-// get an element by ID
-// const title = document.getElementById('page-title'); // a hash '#' is needed if u want to get an id,
-// but only use that when you are using querySelector
-// console.log(title);
+const para = document.querySelector('p');
 
-// get elements by their class name
-// const errors = document.getElementsByClassName('error') // again, don't need a dot '.' because method is
-// already implied that we're getting an element by class name
-// console.log(errors);
-// console.log(errors[0]);
-// errors.forEach(error => { // cannot use foreach in an html collection
-//     console.log(error);
-// })
+// console.log(para.innerText)
+// para.innerText = 'ninjas are awesome!'; // this += appends the text to the 'hello, world'. 
+// an equals '=' overrides it
 
+const paras = document.querySelectorAll('p');
 
-// get elements by their tag name
-const paras = document.getElementsByTagName('p');
-console.log(paras);
-console.log(paras[1]);
+// paras.forEach(para => {
+//     console.log(para.innerText);
+//     para.innerText += ' new text';
+// });
+
+const content = document.querySelector('.content');
+
+// console.log(content.innerHTML)
+// content.innerHTML += '<h2>THIS IS A NEW H2</h2>'; // += to append. = to overwrite
+
+const people = ['mario', 'luigi', 'yoshi'];
+
+people.forEach(person => {
+    content.innerHTML += `<p>${person}</p>`;
+});
