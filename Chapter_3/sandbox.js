d360d2e1d5465e23fa06bfb59f531c327848cfef
Kenneth Agustin <kagustin2@rrc.ca>
2024-01-05 09:13:39 -0600

    if and elseif training

--- Chapter_3/sandbox.js
+++ Chapter_3/sandbox.js
@@ -1,10 +1,24 @@
-// do while loops
+// if statements
+const age = 25;
 
-// do while is basically no matter what, it will run once.
+if(age > 20){
+    console.log('you are over 20 years old');
+}
 
-let i = 5;
+const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
 
-do{
-    console.log('val of i is: ', i);
-    i++;
-} while(i < 5);
+if(ninjas.length > 4){
+    console.log("that's a lot of ninjas") // Use double quotes if you're using an apostrophe (')
+}
+
+// if and else if statements
+
+const password = 'p@ssword';
+
+if(password.length >= 12){
+    console.log('that password is mighty strong');
+} else if(password.length >= 8){
+    console.log('that password is long enough!');
+} else { // if first code block is not true, then move to else if/else statement
+  console.log('password is not long enough')
+}
