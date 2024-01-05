17b935f8d5ebc4a493412c9a1ec0bb6bfd8a420b
Kenneth Agustin <kagustin2@rrc.ca>
2024-01-05 10:44:51 -0600

    returning values code saved

---
+++ Chapter_4/returning_values.js
@@ -0,0 +1,12 @@
+// returning values
+
+// const speak = function(name = 'lugi', time = 'night'){
+//     console.log(`good ${time} ${name}`);
+// };
+
+const calcArea = function(radius){
+    return 3.14 * radius**2;
+};
+
+const a = calcArea(5);
+console.log(a);
