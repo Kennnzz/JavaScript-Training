f7cdde10045cfcd3fdef01aac86263ac71117173
Kenneth Agustin <kagustin2@rrc.ca>
2024-01-05 08:51:16 -0600

    while loops training

--- Chapter_3/sandbox.js
+++ Chapter_3/sandbox.js
@@ -1,21 +1,15 @@
-// for loops
+// while loops
+const names = ['shaun', 'mario', 'luigi'];
+// let i = 0;
 
-// Curly braces is know as code block
-/* 'i' is the variable
-    if 'i' is less than 5, the code block runs.
-    finally, the third parameter, it executes at the end
-    of the code block and repeats, until 'i' is 5 or greater */
-// for(let i = 0; i < 5; i++){
-//     console.log('in loop:', i)
+// while(i < 5){
+//     console.log('in loop: ', i);
+//     i++; // For while loops, to avoid infinite loops, u have to increment 'i'.
+//     // in for loops it was in the parameters, for while, its in the code block
 // }
 
-// // log will run after loop has finished
-// console.log('loop finished');
-
-const names = ['shaun', 'mario', 'luigi']
-
-for(let i = 0; i < names.length; i++){
-    // console.log(names[i]) // names[i] this syntax will print out each of the names, not all of it
-    let html = `<div>${names[i]}</div>`;
-    console.log(html);
+let i = 0;
+while(i < names.length){
+    console.log(names[i]);
+    i++;
 }
