2c5ca47ea2b973036b15deb4f7ad4d180fd15a76
Kenneth Agustin <kagustin2@rrc.ca>
2024-01-05 13:12:30 -0600

    updated object literals code

--- Chapter_5/sandbox.js
+++ Chapter_5/sandbox.js
@@ -5,20 +5,21 @@ let user = {
     age: 30,
     email: 'crystal@thenetninja.co.uk',
     location: 'berlin',
-    blogs: ['why mac & cheese rules', '10 thungs to make with marmite'],
+    blogs: ['why mac & cheese rules', '10 things to make with marmite'],
     login: function(){
         console.log('the user logged in');
     },
     logout: function(){
         console.log('the user logged out')
-    },
-    logBlogs: function(){
-        
+    }, // for shorter version, remove function and colon
+    logBlogs(){ // we cannot use an arow function to do this
+        // console.logBlogs(this.blogs);
+        console.log('this user has written the following blogs:')
+        this.blogs.forEach(blog => {
+            console.log(blog)
+        });
     }
 };
 
-user.login();
-user.logout();
-
-const name = 'mario';
-name.toUpperCase();
+user.logBlogs();
+console.log(this);
