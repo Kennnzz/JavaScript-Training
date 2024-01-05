06792ef6ddfc55179091d4f79f98c5091e5d7afa
Kenneth Agustin <kagustin2@rrc.ca>
2024-01-05 12:00:04 -0600

    obj literals pt2 training

--- Chapter_5/sandbox.js
+++ Chapter_5/sandbox.js
@@ -5,23 +5,20 @@ let user = {
     age: 30,
     email: 'crystal@thenetninja.co.uk',
     location: 'berlin',
-    blogs: ['why mac & cheese rules', '10 thungs to make with marmite'] 
+    blogs: ['why mac & cheese rules', '10 thungs to make with marmite'],
+    login: function(){
+        console.log('the user logged in');
+    },
+    logout: function(){
+        console.log('the user logged out')
+    },
+    logBlogs: function(){
+        
+    }
 };
 
-console.log(user);
+user.login();
+user.logout();
 
-// Acess properties
-console.log(user.name);
-
-// Update properties
-user.age = 35
-console.log(user.age);
-
-const key = 'location';
-
-// Access any property with square bracket notation
-console.log(user['name']);
-user['name'] = 'chun-li' // Another way to update name
-console.log(user['name']) // now this outputs chun-li instead of crystal
-
-console.log(typeof user); // outputs object since this is of object type
+const name = 'mario';
+name.toUpperCase();
