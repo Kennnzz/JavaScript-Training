9e0b9e795ae2f9f636257ea96ff2e3cfc653e322
Kenneth Agustin <kagustin2@rrc.ca>
2024-01-05 10:06:42 -0600

    blockscopes and nested block scopes training

--- Chapter_3/sandbox.js
+++ Chapter_3/sandbox.js
@@ -1,59 +1,17 @@
-// break and continue
+// variables & block scope
+let age = 30;
 
-const scores = [50, 25, 0, 100, 20 ,10];
+if(true){
+    // age = 40; // changes age to 40 and outside age to 40. overwriting changes the global variable
+    let age = 40; // changes only the inside to 40. redefining instead of overwriting changes inside variable
+    let name = 'shaun'; // defininig a variable inside a code block makes a local scope only accessible...
+    // inside the code block
+    console.log('inside 1st code block: ', age, name);
 
-for(let i = 0; i < scores.length; i++){
-
-    if(scores[i] === 0){
-        continue; // continue makes it so we basically skip whatever is in the argument
-        // when u run this, 0 should not be logged into the console anymore
-    }
-
-    console.log('your score: ', scores[i]);
-
-    if(scores[i] === 100){
-        console.log('congrats, you got the top score!');
-        break; // break keyword breaks you out of the loop. upon reaching 100, it does not do 20 and 10 anymore
+    if(true){ // nested code block still uses the most recent value outside of its block, which is 40
+        let age = 50; // created a new local variable that only works inside nested block
+        console.log('inside 2nd code block: ', age)
     }
 }
 
-// switch statements
-const grade = 'z';
-
-// remeber, switches use strict equality (===) so putting in a number will not work.
-switch(grade){
-    case 'A':
-        console.log('you got an A!');
-        break;
-    case 'B':
-        console.log('you got an B!');
-        break;
-    case 'C':
-        console.log('you got an C!');
-        break;
-    case 'D':
-        console.log('you got an D!');
-        break;
-    case 'E':
-        console.log('you got an E!');
-        break;
-    default:
-        console.log('not a valid grade')
-}
-
-// use case instead of \/
-
-// using if statements
-// if(grade === 'A'){
-
-// } else if(grade === 'B'){
-
-// } else if(grade === 'C'){
-
-// } else if(grade === 'D'){
-    
-// } else if(grade === 'E'){
-
-// } else {
-
-// }
+console.log('outside code block: ', age);
